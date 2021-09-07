3fae138d0cc0fe45723344f4a618dd1f36784466
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-08 01:25:12 +0400

    Добавьте файлы проекта.

---
+++ IncreaseNumberRoundness_CodeSignal_Core_Test31/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncreaseNumberRoundness_CodeSignal_Core_Test31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(increaseNumberRoundness(input));
+        }
+
+        static bool increaseNumberRoundness(int n)
+        {
+            int count = 0;
+            List<int> list = new List<int>();
+            while (n > 0)
+            {
+                list.Add(n % 10);
+                n = n / 10;
+            }
+            list.Reverse();
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] == 0 && list[i + 1] != 0)
+                {
+                    int temp = list[i + 1];
+                    list[i + 1] = list[i];
+                    list[i] = temp;
+                    count++;
+                }
+            }
+            if (count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
